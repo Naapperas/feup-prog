8a58629c347b2a6b959b43178b4daca37d471c18
Nuno Pereira <nunoafonso2002@gmail.com>
2021-03-12 16:37:41 +0000

    Finished ex16

--- p2/ex16.cpp
+++ p2/ex16.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -12,43 +13,75 @@ int main() {
 
     string calculatorOperation;
 
-    cout << "A - perform basic arithmetic" << endl << "M - store last value in calculator memory" << endl << "MC - clear memory contents" << endl << "MR - show contents of memory on display" << endl;
+    cout << "A\tperform basic arithmetic\nM\tstore last value in calculator memory\nM-\tsubtract from the last value in calculator memory\nM+\tadd to the last value in calculator memory\nMC\tclear memory contents\nMR\tshow contents of memory on display\nC\tclear the contents of the screen\nQ\tquit calulator" << endl;
     do {
-        
-        cout << "Please write the operation to perform: " ;
-        cin >> calculatorOperation;
-
-        if (calculatorOperation == "A") {
-            
-            continue;
-
-        } else if (calculatorOperation == "M") {
-            
-            continue;
-
-        } else if (calculatorOperation == "MC") {
-            
-            memory.clear();
-            cout << "Cleared memory" << endl;
-            continue;
-
-        } else if (calculatorOperation == "MR") {
-            
-            for (int i = 0; i < memory.size(); i++) {
-                cout << "Mem[" << i << "] = " << memory[i] << ';' << endl;
-            }
-            continue;
-            
-        } else {
-            
-            cout << "Invalid operation, please try again." << endl;
-            continue;
-        }
 
         double op1, op2;
         char operation;
 
-        //TODO: parse input correctly
+        cout << "Please write the operation to perform: " ;
+        cin >> calculatorOperation;
+        if (calculatorOperation == "Q" || calculatorOperation == "q"){
+            break;
+        } else if (calculatorOperation == "A" || calculatorOperation == "a") {
+            
+            string op1s, operations, op2s;
+            cin >> op1s >> operations >> op2s;
+
+            if (operations != "+" && operations != "-" && operations != "*" && operations != "/") {
+                cout << "Erro: operação inválida" << endl;
+                answer = 'y';
+                continue;
+            }
+            
+            op1 = stoi(op1s);
+            operation = operations[0];
+            op2 = stoi(op2s);
+
+        } else if (calculatorOperation == "M" || calculatorOperation == "m") {
+            
+            memory.push_back(result);
+            answer = 'y';
+            continue;
+
+        } else if (calculatorOperation == "M+" || calculatorOperation == "m+") {
+
+            memory[memory.size() - 1] += result; // maybe there is a better way to implement this
+            answer = 'y';
+            continue;
+
+        } else if (calculatorOperation == "M-" || calculatorOperation == "m-") {
+
+            memory[memory.size() - 1] -= result;
+            answer = 'y';
+            continue;
+
+        } else if (calculatorOperation == "MC" || calculatorOperation == "mc") {
+            
+            memory.clear();
+            cout << "Cleared memory" << endl;
+            answer = 'y';
+            continue;
+
+        } else if (calculatorOperation == "MR" || calculatorOperation == "mr") {
+            
+            for (int i = 0; i < memory.size(); i++) {
+                cout << "Mem[" << i << "] = " << memory[i] << ';' << endl;
+            }
+            answer = 'y';
+            continue;
+            
+        } else if (calculatorOperation == "C" || calculatorOperation == "c") {
+            for (int i = 0; i < 20; i++) cout << '\n';
+            cout << endl;
+            answer = 'y';
+            continue;
+        } else {
+            
+            cout << "Invalid operation, please try again." << endl;
+            answer = 'y';
+            continue;
+        }
 
         switch (operation) {
 
@@ -73,10 +106,6 @@ int main() {
         }
 
         cout << op1 << " " << operation << " " << op2 << " = " << result << endl;
-
-        cout << "What memory changes do you want to make ? (or 'none' if no memory changes should be made)";
-
-        //TODO: memory saving options
 
         cout << "Do you want to make any more operations(y/n/Y/N)? "; 
         cin >> answer;
